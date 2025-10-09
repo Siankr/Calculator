743db1eeabd8bbb941b9a98b8041793c4b2d4e4e
Siankr <srowlands@vmg.com.au>
2025-10-10 09:56:03 +1100

    Create run_examples.js

---
+++ rules/duty/2025-26/src/run_examples.js
@@ -0,0 +1,18 @@
+const { solveMaxPrice } = require("./purchasingPower");
+const { calcDutyNSW } = require("./duty");
+
+console.log("Duty on $900,000 (NSW, non-FHB):", calcDutyNSW({ price: 900000, isFhb: false }));
+console.log("Duty on $800,000 (NSW, FHB home):", calcDutyNSW({ price: 800000, isFhb: true }));
+
+const scenario = {
+  borrowingPower: 700000,
+  depositCash: 180000,
+  targetLvr: 0.90,
+  lmiPolicy: "allow_lmi", // try "no_lmi" or "fhb_guarantee"
+  isFhb: true,
+  isLand: false,
+  includeOtherGovtFees: true,
+  contractDate: "2025-10-10",
+  region: "capital_and_regional_centres"
+};
+console.log("Max price (scenario):", solveMaxPrice(scenario));
