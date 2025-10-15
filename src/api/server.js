127d1549867d616e2aabc6a0529d84d164662af4
Siankr <srowlands@vmg.com.au>
2025-10-15 13:59:19 +1100

    Update server.js

--- src/api/server.js
+++ src/api/server.js
@@ -197,6 +197,5 @@ app.get('/', (_req, res) => {
 
 /** Start server */
 const PORT = process.env.PORT || 8787;
-app.listen(PORT, () => {
-  console.log(`[calculator-api] listening on :${PORT}`);
-});
+app.listen(PORT, '0.0.0.0', () => console.log(`[calculator-api] listening on :${PORT}`));
+
